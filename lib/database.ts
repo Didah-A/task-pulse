5bcd1f542291ba0193eeacc6c61f8a0459c0073d
Didah <aseeydidah@gmail.com>
2024-05-16 11:06:49 +0300

    add authentication provider and adapters

--- lib/database.ts
+++ lib/database.ts
@@ -1,25 +1,5 @@
 import mongoose from "mongoose";
 
-// let isConnected = false;
-
-// export const connectToDB = async () => {
-//   mongoose.set("strictQuery", true);
-
-//   if (isConnected) {
-//     console.log("Already connected!");
-//     return;
-//   }
-//   try {
-//     await mongoose.connect(process.env.MONGODB_URI!, {
-//       dbName: "taskPulse",
-//     });
-//     isConnected = true;
-//     console.log("connected");
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
